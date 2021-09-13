a65ff2a8758d6ea8e5d32c016f5f22c66baafa73
Edward Du <edwarddu1234@gmail.com>
2021-09-12 22:18:06 -0400

    Add project files.

---
+++ BillSplit/Data/BillItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillSplit.Data
+{
+    public class BillItem
+    {
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is Required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Enter a Positive Number")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "People Paying are Required")]
+        public List<string> Names { get; set; }
+    }
+}
